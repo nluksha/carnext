be23724179e3c8d1eec4b5a1c79eac59ac4cb9e7
Mikita Luksha <nikita.luksha@gmail.com>
2019-05-09 16:27:15 +0300

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": ["react-app", "airbnb-base", "prettier"],
+  "plugins": ["prettier", "react", "jsx-a11y", "import"],
+  "rules": {
+    "prettier/prettier": ["error", {
+      "singleQuote": true,
+      "printWidth": 120
+    }],
+    "consistent-return": 0,
+    "linebreak-style": 0,
+    "no-trailing-spaces": 0
+  }
+};
